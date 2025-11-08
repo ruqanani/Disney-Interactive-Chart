853264accf42897d338b46240dc6a071ab663cd7
hassanruqa <182183409+hassanruqa@users.noreply.github.com>
2025-11-08 13:24:40 -0500

    Enhance data processing in script.js
    Added logic to create an array of years from 1980 to 2020 in increments of 5 and log it to the console.

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 
-    d3.csv("data/disney.csv", function(data)
-    {
-        var filtered_data = data.filter(function(d) { 
-            return d.type === "TV Show" && d.rating.startsWith("TV");
-        });
-    
-
-    
-    
-    
-    
+d3.csv("data/disney.csv", function(data)
+{
+    var filtered_data = data.filter(function(d) { 
+        return d.type === "TV Show" && d.rating.startsWith("TV");
     });
 
+var years = [];
+for (var y = 1980; y <= 2020; y+=5){
+    years.push(y);
+}
+console.log(years);
 
+filtered_data.forEach(function(d) {
     
+
+});
+
+
+
+});
+
+
+
+
 
 
 
