c7653ed8043f5f91629a5280006edecee4804f96
hassanruqa <182183409+hassanruqa@users.noreply.github.com>
2025-11-09 11:40:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,17 +8,34 @@ d3.csv("data/disney.csv", function(data)
 var years = [];
 for (var y = 1980; y <= 2020; y+=5){
     years.push(y);
-}
-console.log(years);
+} // end of for loop
 
-filtered_data.forEach(function(d) {
+var ratings = ["TV-Y", "TV-Y7", "TV-Y7-FV", "TV-G", "TV-PG", "TV-14"];
+
+var allYearsData = years.map(function(year){
+
+    var yearData = filtered_data.filter(function(d){
+        return +d.release_year === year;
+    }); // end of yearData
+
+    var ratingCounts = ratings.map(function(rating)
+        {
+        var count = yearData.filter(function(d){
+            return d.rating === rating;
+            }).length;
+            
+        return {rating: rating, count: count};
+            
+        }); // end of rating counts
     
+    return {year: year, counts: ratingsCounts};
+        
+    }); // end of allYearsData
 
-});
+   console.log(allYearsData);
 
+}); // end of dc.csv
 
-
-});
 
 
 
