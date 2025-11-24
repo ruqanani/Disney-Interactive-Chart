076c7c061272fd307a7d039f5f9d32e9e56a604e
hassanruqa <182183409+hassanruqa@users.noreply.github.com>
2025-11-24 12:48:12 -0500

    Refactor data processing and visualization logic

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ var hideTooltip = function(d) {
 
     var initialData = allYearsData[0].counts;
 
-    var margin = {top: 40, right: 20, bottom: 50, left: 50};
+    var margin = {top: 40, right: 30, bottom: 70, left: 60};
     var width = 700 - margin.left - margin.right;
     var height = 500 - margin.top - margin.bottom;
 
@@ -148,6 +148,7 @@ var hideTooltip = function(d) {
     drawBarChart(allYearsData[0].counts);
 
 }); // end of dc.csv
+
 
 
 
